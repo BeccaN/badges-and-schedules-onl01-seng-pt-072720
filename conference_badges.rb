11c5de0b11f683e574740bb1b5c062d7855ff895
Rebecca Congi <rncongi@gmail.com>
2020-08-02 22:16:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,23 +4,24 @@ end
 
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def batch_badge_creator(attendees)
-  badges = []
-  attendees.each do |attendee|
-    badges.push("Hello, my name is #{attendee}.") 
-  end
-  return badges
-end 
 
-def assign_rooms(attendees)
-  room_assignments = []
-  num = 1
-  attendees.each do |attendee|
-    room_assignments.push("Hello, #{attendee}! You'll be assigned to room #{num}!")
-    num += 1 
+  def batch_badge_creator(attendees)
+    badges = []
+    attendees.each do |attendee|
+      badges.push("Hello, my name is #{attendee}.") 
+    end
+    return badges
+  end 
+
+  def assign_rooms(attendees)
+    room_assignments = []
+    num = 1
+    attendees.each do |attendee|
+      room_assignments.push("Hello, #{attendee}! You'll be assigned to room #{num}!")
+      num += 1 
+    end
+    return room_assignments
   end
-  return room_assignments
-end
 
 
 
